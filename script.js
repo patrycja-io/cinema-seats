232e1541783a91346482eb0e3a14fc97beb627d0
Patrycja <patrycjatyra@protonmail.com>
2020-02-28 15:14:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,29 @@ const movieSelect = document.getElementById('movie');
 
 const ticketPrice = +movieSelect.value;
 
+// Update total and count
+
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Movie select event
+
+movieSelect.addEventListener('change', (e) => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+});
+
+// Seat click event
+
 container.addEventListener('click', (e) => {
     if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
         e.target.classList.toggle('selected');
+        updateSelectedCount();
     }
 });
